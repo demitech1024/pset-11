9ee765beefbc4079fbb67459b462f67393c5e91b
demitech1024 <demitech1024@gmail.com>
2020-03-22 19:42:57 -0400

    2

--- src/Exercises.java
+++ src/Exercises.java
@@ -14,10 +14,34 @@ public class Exercises {
   }
 
   public int findMe(ArrayList<String> list, String target) {
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i) == target) {
+        return i;
+      }
+    }
+
     return -1;
   }
 
   public int findMeFaster(ArrayList<Integer> list, int target) {
+    if (list == null) {
+      return -1;
+    }
+    
+    int start = 0;
+    int end = list.size() - 1;
+    int middle = 0;
+    while (start <= end) {
+      middle = (start + end) / 2;
+      if (list.get(middle) == target) {
+        return middle;
+      } else if (list.get(middle) > target) {
+        end = middle - 1;
+      } else if (list.get(middle) < target) {
+        start = middle + 1;
+      }
+    }
+
     return -1;
   }
 
